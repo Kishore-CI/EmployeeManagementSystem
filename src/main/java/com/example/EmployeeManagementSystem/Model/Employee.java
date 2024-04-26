1e9b6a04834dae4603e1e9a4fe823488d348bcbd
Kishore-97 <lkk9701@gmail.com>
2024-04-26 18:46:44 +0530

    Third commit

--- src/main/java/com/example/EmployeeManagementSystem/Model/Employee.java
+++ src/main/java/com/example/EmployeeManagementSystem/Model/Employee.java
@@ -54,5 +54,51 @@ public class Employee {
         this.salary = salary;
     }
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
